e3cf67e4f464bab49737d6aab2468e977b2b6829
landmineHQ <z1216198007@gmail.com>
2024-12-07 08:07:13 +0800

    init: 首次初始化

---
+++ overrides/README.md
@@ -0,0 +1,2 @@
+https://github.com/Saudade-Studio-RU/Desiderium-Dedicated-Server
+Выделеный сервер можн скачать тут
---
+++ overrides/kubejs/client_scripts/tooltips.js
@@ -0,0 +1,4 @@
+onEvent('item.tooltip', tooltip => {
+	 tooltip.remove('ars_nouveau:wilden_wing')
+	 tooltip.add('ars_nouveau:wilden_wing', '在吸收室中用蝙蝠翅膀制作而成。')
+})
